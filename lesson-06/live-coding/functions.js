965f3a3bcf460fff27295bc3dbce5e7de083e63c
Basti Buck <mail@bastibuck.de>
2021-05-17 19:49:41 +0200

    lesson 06

---
+++ lesson-06/live-coding/functions.js
@@ -0,0 +1,9 @@
+const age = 31;
+
+const isRetired = age > 67;
+
+// wenn rentner, dann eintrittspreis reduzieren
+console.log(isRetired);
+
+// Elemente zugreife, die im HTML sind
+// finde den <button>... gibt es nicht, fehler
